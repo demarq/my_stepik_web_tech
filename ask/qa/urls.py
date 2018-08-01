4e00e20ee28fd5419d535e2869cf42fa16ac766e
demarq <pro100play@gmail.com>
2018-08-01 08:16:18 +0000

    success file structure for lesson 2.1

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 from qa.views import test
 
 urlpatterns = [
-    url(r'', test, name='home'),
+    url(r'^$', test, name='home'),
     url(r'login/', test, name='login'),
-    url(r'question/(?P<id>[\d]{0,10})&', test, name='question'),
+    url(r'question/(?P<id>[\d]{0,10})$', test, name='question'),
     url(r'ask/', test, name='ask'),
     url(r'popular/', test, name='popular'),
     url(r'new/', test, name='new'),
