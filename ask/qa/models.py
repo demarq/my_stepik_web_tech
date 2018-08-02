7a33c77288d2d68e9df0686c1551f941a32a5472
Valenin <pro100play@gmail.com>
2018-08-02 16:18:04 +0300

    added questionmanager to models

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,7 +2,14 @@ from django.db import models
 
 # Create your models here.
 
+class QuestionManager(models.Manager):
+        def new():
+                return self.order_by('-added_at')
+        def popular():
+                return self.order_by('-rating')
+
 class Question(models.Model):
+    objects = QuestionManager()
     title = models.CharField()
     text = models.TextField()
     added_at = models.DateTimeField()
