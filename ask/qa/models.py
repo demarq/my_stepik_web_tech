fd6555ba8246ce16d71777bb1774580785259667
Valenin <pro100play@gmail.com>
2018-08-02 16:12:19 +0300

    added models in django and init_mysql script

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    title = models.CharField()
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.CharField()
+    likes = models.Charfied()
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question, on_delete=SET_NULL)
+    author = models.CharField()
---
+++ init_mysql.sh
@@ -0,0 +1,1 @@
+mysql -uroot -pqwerted -e "create database django; create user django identified by ''; grant all on django.* to django"
